f94c5cd0badd2b30892880a4a05c1eccbb3fff99
jsfoon <jiasheng@pantheonlab.ai>
2022-11-28 13:27:57 +0800

    TN-123 remove changelog config file

--- change-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const config = require("conventional-changelog-conventionalcommits");
-
-module.exports = config({
-  issuePrefixes: ["TN-"],
-  issueUrlFormat: "https://jira.example.com/browse/{{prefix}}{{id}}",
-});
